bc9daa3af9f3c7cc09d9934b22851c8fbdaac9ca
dimav <dimavg200>
2023-06-27 19:26:43 +0300

    Some updates

--- QML/QML Course/coding_assignments/assignment_helper.py
+++ QML/QML Course/coding_assignments/assignment_helper.py
@@ -141,7 +141,8 @@ if __name__ == "__main__":
         except ImportError:
             from qiskit import BasicAer as Aer
         from qiskit.quantum_info import Pauli
-        from qiskit.aqua.operators import *
+        from qiskit.aqua import Operator, get_aer_backend
+        from qiskit.aqua.components.initial_states import Custom
         is_qiskit = True
     except ImportError:
         is_qiskit = False
